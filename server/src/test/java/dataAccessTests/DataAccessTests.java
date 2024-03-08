771da8eb2aa1bdcd6fe17ab4a545409ae411c1fa
Ethan M. Blaylock <ethan.blaylock37@gmail.com>
2024-03-07 20:01:17 -0700

    Phase 4: Added some new tests

--- server/src/test/java/dataAccessTests/DataAccessTests.java
+++ server/src/test/java/dataAccessTests/DataAccessTests.java
@@ -1,14 +1,12 @@
 package dataAccessTests;
 
 import dataAccess.*;
+import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import service.ClearService;
-
-import javax.xml.crypto.Data;
 import java.util.Collections;
 
 public class DataAccessTests {
@@ -25,27 +23,109 @@ public class DataAccessTests {
         DatabaseManager.truncateTable("gameData");
     }
     @Test
-    @DisplayName("Clear the Application positive")
-    public void clearTestPositive() throws DataAccessException {
+    @DisplayName("Create User")
+    public void createUserPositive() throws DataAccessException {
         UserDAO.createUser(new UserData("JimBob", "password", "arwals@g.com"));
-        AuthDAO.createAuth("JimBob");
-        GameDAO.createGame("i love chess");
-        ClearService.clearApplication();
-        Assertions.assertEquals(Collections.emptySet(), UserDAO.getData());
-        Assertions.assertEquals(Collections.emptySet(), AuthDAO.getData());
-        Assertions.assertEquals(Collections.emptySet(), GameDAO.getData());
-
+        Assertions.assertNotNull(UserDAO.getData());
     }
 
     @Test
-    @DisplayName("Clear the Application positive")
+    @DisplayName("Username taken")
+    public void createUserNegative() throws DataAccessException {
+        UserData user = new UserData("JimBob", "password", "arwals@g.com");
+        UserDAO.createUser(user);
+        Assertions.assertThrows(DataAccessException.class, () -> UserDAO.createUser(user));
+    }
+
+    @Test
+    @DisplayName("Correct password")
+    public void checkPasswordPositive() throws DataAccessException {
+        UserData user = new UserData("JimBob", "password", "arwals@g.com");
+        UserDAO.createUser(user);
+        Assertions.assertTrue(UserDAO.checkPassword(user.username(), user.password()));
+    }
+
+    @Test
+    @DisplayName("Wrong password")
+    public void checkPasswordNegative() throws DataAccessException {
+        UserData user = new UserData("JimBob", "password", "arwals@g.com");
+        UserDAO.createUser(user);
+        String fakePassword = "fake password";
+        Assertions.assertFalse(UserDAO.checkPassword(user.username(), fakePassword));
+    }
+
+    @Test
+    @DisplayName("Get users")
     public void getUserTestPositive() throws DataAccessException {
         UserData expectedUser = new UserData("JimBob", "password", "arwals@g.com");
         UserDAO.createUser(expectedUser);
         UserData userData = UserDAO.getUser("JimBob");
+        assert userData != null;
         Assertions.assertEquals(expectedUser.email(), userData.email());
         Assertions.assertEquals(expectedUser.username(), userData.username());
+    }
 
+    @Test
+    @DisplayName("No users")
+    public void getUserTestNegative() throws DataAccessException {
+        UserData userData = UserDAO.getUser("JimBob");
+        Assertions.assertNull(userData);
+    }
+
+    @Test
+    @DisplayName("Clear users")
+    public void userClear() throws DataAccessException {
+        UserData expectedUser = new UserData("JimBob", "password", "arwals@g.com");
+        UserDAO.createUser(expectedUser);
+        UserDAO.clear();
+        Assertions.assertEquals(UserDAO.getData(), Collections.emptySet());
+    }
+
+    @Test
+    @DisplayName("Get the Data")
+    public void getUserDataPositive() throws DataAccessException {
+        UserData expectedUser = new UserData("JimBob", "password", "arwals@g.com");
+        UserDAO.createUser(expectedUser);
+        Assertions.assertNotNull(UserDAO.getData());
+    }
+
+    @Test
+    @DisplayName("No data")
+    public void getUserDataNegative() throws DataAccessException {
+        Assertions.assertEquals(UserDAO.getData(), Collections.emptySet());
+    }
+
+    @Test
+    @DisplayName("Create Auth")
+    public void createAuthPositive() throws DataAccessException {
+        AuthData actual = AuthDAO.createAuth("JimBob");
+        Assertions.assertNotNull(actual);
+    }
+
+    @Test
+    @DisplayName("Create Auth Negative")
+    public void createAuthNegative() throws DataAccessException {
+        AuthData first = AuthDAO.createAuth("JimBob");
+        AuthData second = AuthDAO.createAuth("JimBob");
+        Assertions.assertNotEquals(first, second);
+    }
+
+    @Test
+    @DisplayName("Get users")
+    public void getAuthTestPositive() throws DataAccessException {
+        UserData expectedUser = new UserData("JimBob", "password", "arwals@g.com");
+        UserDAO.createUser(expectedUser);
+        UserData userData = UserDAO.getUser("JimBob");
+        assert userData != null;
+        Assertions.assertEquals(expectedUser.email(), userData.email());
+        Assertions.assertEquals(expectedUser.username(), userData.username());
+    }
+
+    @Test
+    @DisplayName("No users")
+    public void getAuthTestNegative() throws DataAccessException {
+        UserData userData = UserDAO.getUser("JimBob");
+        Assertions.assertNull(userData);
     }
 
     @Test
