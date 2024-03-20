fb132c6e24701e5854c5518d3ba8507753fbe687
Ethan M. Blaylock <ethan.blaylock37@gmail.com>
2024-03-19 20:50:27 -0600

    Phase 5: Made some progress

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,166 @@
 import chess.*;
+import ui.EscapeSequences;
+
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Client. Type Help to get started. ♕ \n" );
+        String currentState = "[LOGGED OUT] >>> ";
+        Scanner scanner = new Scanner(System.in);
+        boolean quitProgram = false;
+        while (!quitProgram) {
+            System.out.print(currentState);
+            String command = scanner.nextLine();
+            command = command.toLowerCase();
+            switch(command) {
+                case "help":
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE);
+                    System.out.print("  register ");
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_MAGENTA);
+                    System.out.println("- to create an account");
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE);
+                    System.out.print("  login ");
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_MAGENTA);
+                    System.out.println("- to play chess");
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE);
+                    System.out.print("  quit ");
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_MAGENTA);
+                    System.out.println("- playing chess");
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE);
+                    System.out.print("  help ");
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_MAGENTA);
+                    System.out.println("- with possible commands");
+                    System.out.print(EscapeSequences.RESET_TEXT_COLOR);
+                    break;
+                case "quit":
+                    quitProgram = true;
+                    System.out.print("  Thank you for playing!");
+                    break;
+                case "login":
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE);
+                    System.out.print("Username: ");
+                    String username = scanner.nextLine();
+                    System.out.print("Password: ");
+                    String password = scanner.nextLine();
+                    System.out.println("asfdsa");
+                    System.out.print(EscapeSequences.RESET_TEXT_COLOR);
+
+                    break;
+                case "register":
+                    ChessBoard board = new ChessBoard();
+                    board.resetBoard();
+                    makeChessBoard(board);
+                    break;
+            }
+        }
     }
+
+
+    static void makeChessBoard(ChessBoard board) {
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_DARK_GREY);
+        int flipper = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.print(EscapeSequences.SET_BG_COLOR_GREY);
+            if (i > 0 && i < 9) {
+                char c = (char)(i+96);
+                System.out.print(" " + c + " ");
+            } else {
+                System.out.print(EscapeSequences.EMPTY);
+            }
+        }
+        System.out.println(EscapeSequences.RESET_BG_COLOR);
+        for (int i = 8; i >= 1; i--) {
+            System.out.print(EscapeSequences.SET_BG_COLOR_GREY);
+            System.out.print(" " + i + " ");
+            for (int j = 1; j <= 8; j++) {
+                if (flipper == 1) {
+                    System.out.print(EscapeSequences.SET_BG_COLOR_GREEN);
+                    if (board.getPiece(new ChessPosition(i, j)) != null) {
+                        System.out.print(board.getPiece(new ChessPosition(i, j)));
+                    } else {
+                        System.out.print(EscapeSequences.EMPTY);
+                    }
+
+                } else {
+                    System.out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
+                    if (board.getPiece(new ChessPosition(i, j)) != null) {
+                        System.out.print(board.getPiece(new ChessPosition(i, j)));
+                    } else {
+                        System.out.print(EscapeSequences.EMPTY);
+                    }
+                }
+                flipper = flipper ^ 1;
+            }
+            System.out.print(EscapeSequences.SET_BG_COLOR_GREY);
+            System.out.print(" " + i + " ");
+            System.out.println(EscapeSequences.RESET_BG_COLOR);
+            flipper = flipper ^ 1;
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.print(EscapeSequences.SET_BG_COLOR_GREY);
+            if (i > 0 && i < 9) {
+                char c = (char)(i+96);
+                System.out.print(" " + c + " ");
+            } else {
+                System.out.print(EscapeSequences.EMPTY);
+            }
+        }
+
+        System.out.println(EscapeSequences.RESET_BG_COLOR);
+        System.out.println(EscapeSequences.RESET_BG_COLOR);
+
+        for (int i = 9; i >= 0; i--) {
+            System.out.print(EscapeSequences.SET_BG_COLOR_GREY);
+            if (i > 0 && i < 9) {
+                char c = (char)(i+96);
+                System.out.print(" " + c + " ");
+            } else {
+                System.out.print(EscapeSequences.EMPTY);
+            }
+        }
+        System.out.println(EscapeSequences.RESET_BG_COLOR);
+        for (int i = 1; i <= 8; i++) {
+            System.out.print(EscapeSequences.SET_BG_COLOR_GREY);
+            System.out.print(" " + i + " ");
+            for (int j = 8; j >= 1; j--) {
+                if (flipper == 1) {
+                    System.out.print(EscapeSequences.SET_BG_COLOR_GREEN);
+                    if (board.getPiece(new ChessPosition(i, j)) != null) {
+                        System.out.print(board.getPiece(new ChessPosition(i, j)));
+                    } else {
+                        System.out.print(EscapeSequences.EMPTY);
+                    }
+
+                } else {
+                    System.out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
+                    if (board.getPiece(new ChessPosition(i, j)) != null) {
+                        System.out.print(board.getPiece(new ChessPosition(i, j)));
+                    } else {
+                        System.out.print(EscapeSequences.EMPTY);
+                    }
+                }
+                flipper = flipper ^ 1;
+            }
+            System.out.print(EscapeSequences.SET_BG_COLOR_GREY);
+            System.out.print(" " + i + " ");
+            System.out.println(EscapeSequences.RESET_BG_COLOR);
+            flipper = flipper ^ 1;
+        }
+        for (int i = 9; i >= 0; i--) {
+            System.out.print(EscapeSequences.SET_BG_COLOR_GREY);
+            if (i > 0 && i < 9) {
+                char c = (char)(i+96);
+                System.out.print(" " + c + " ");
+            } else {
+                System.out.print(EscapeSequences.EMPTY);
+            }
+        }
+
+        System.out.println(EscapeSequences.RESET_BG_COLOR);
+        System.out.println(EscapeSequences.RESET_TEXT_COLOR);
+    }
+
+
 }
--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -26,26 +26,28 @@ public class EscapeSequences {
 
     public static final String SET_TEXT_COLOR_BLACK = SET_TEXT_COLOR + "0m";
     public static final String SET_TEXT_COLOR_LIGHT_GREY = SET_TEXT_COLOR + "242m";
-    public static final String SET_TEXT_COLOR_DARK_GREY = SET_TEXT_COLOR + "235m";
+    public static final String SET_TEXT_COLOR_DARK_GREY = SET_TEXT_COLOR + "239m";
     public static final String SET_TEXT_COLOR_RED = SET_TEXT_COLOR + "160m";
     public static final String SET_TEXT_COLOR_GREEN = SET_TEXT_COLOR + "46m";
     public static final String SET_TEXT_COLOR_YELLOW = SET_TEXT_COLOR + "226m";
     public static final String SET_TEXT_COLOR_BLUE = SET_TEXT_COLOR + "12m";
     public static final String SET_TEXT_COLOR_MAGENTA = SET_TEXT_COLOR + "5m";
     public static final String SET_TEXT_COLOR_WHITE = SET_TEXT_COLOR + "15m";
-    public static final String RESET_TEXT_COLOR = SET_TEXT_COLOR + "0m";
+    public static final String RESET_TEXT_COLOR = ANSI_ESCAPE + "[39m";
 
     public static final String SET_BG_COLOR_BLACK = SET_BG_COLOR + "0m";
-    public static final String SET_BG_COLOR_LIGHT_GREY = SET_BG_COLOR + "242m";
+    public static final String SET_BG_COLOR_LIGHT_GREY = SET_BG_COLOR + "230m";
+
+    public static final String SET_BG_COLOR_GREY = SET_BG_COLOR + "253m";
     public static final String SET_BG_COLOR_DARK_GREY = SET_BG_COLOR + "235m";
     public static final String SET_BG_COLOR_RED = SET_BG_COLOR + "160m";
-    public static final String SET_BG_COLOR_GREEN = SET_BG_COLOR + "46m";
+    public static final String SET_BG_COLOR_GREEN = SET_BG_COLOR + "107m";
     public static final String SET_BG_COLOR_DARK_GREEN = SET_BG_COLOR + "22m";
     public static final String SET_BG_COLOR_YELLOW = SET_BG_COLOR + "226m";
     public static final String SET_BG_COLOR_BLUE = SET_BG_COLOR + "12m";
     public static final String SET_BG_COLOR_MAGENTA = SET_BG_COLOR + "5m";
     public static final String SET_BG_COLOR_WHITE = SET_BG_COLOR + "15m";
-    public static final String RESET_BG_COLOR = SET_BG_COLOR + "0m";
+    public static final String RESET_BG_COLOR = ANSI_ESCAPE + "[49m";
 
     public static final String WHITE_KING = " ♔ ";
     public static final String WHITE_QUEEN = " ♕ ";
