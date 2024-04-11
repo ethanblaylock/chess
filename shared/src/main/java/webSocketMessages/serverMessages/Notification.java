557bbef72fb8740e39b2edc2a304bce5151ea517
Ethan M. Blaylock <ethan.blaylock37@gmail.com>
2024-04-11 14:32:01 -0600

    Phase 6: Added the new classes for the servermessages

---
+++ shared/src/main/java/webSocketMessages/serverMessages/Notification.java
@@ -0,0 +1,9 @@
+package webSocketMessages.serverMessages;
+
+public class Notification extends ServerMessage{
+    private String message;
+    public Notification(String message) {
+        super(ServerMessageType.NOTIFICATION);
+        this.message = message;
+    }
+}
