19dac5e8d1a270d65d508995896a8d8513665962
Ethan M. Blaylock <ethan.blaylock37@gmail.com>
2024-04-11 21:14:48 -0600

    Phase 6: Updated for new functionality

--- shared/src/main/java/webSocketMessages/userCommands/JoinPlayer.java
+++ shared/src/main/java/webSocketMessages/userCommands/JoinPlayer.java
@@ -4,10 +4,17 @@ import chess.ChessGame;
 
 public class JoinPlayer extends UserGameCommand{
     private ChessGame.TeamColor playerColor;
-
-    public JoinPlayer(String authToken, int gameID, ChessGame.TeamColor playerColor) {
+    private String username;
+    public JoinPlayer(String authToken, int gameID, ChessGame.TeamColor playerColor, String username) {
         super(authToken, gameID);
         super.commandType = CommandType.JOIN_PLAYER;
         this.playerColor = playerColor;
+        this.username = username;
     }
+
+    public ChessGame.TeamColor getPlayerColor() {
+        return playerColor;
+    }
+
+    public String getUsername() { return username; }
 }
--- shared/src/main/java/webSocketMessages/userCommands/Leave.java
+++ shared/src/main/java/webSocketMessages/userCommands/Leave.java
@@ -1,6 +1,7 @@
 package webSocketMessages.userCommands;
 
 public class Leave extends UserGameCommand{
+
     public Leave(String authToken, int gameID) {
         super(authToken, gameID);
         commandType = CommandType.LEAVE;
