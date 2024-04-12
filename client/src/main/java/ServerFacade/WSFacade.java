c94cb1cc92f13e9ff9065370058e2e21c5b01214
Ethan M. Blaylock <ethan.blaylock37@gmail.com>
2024-04-11 23:51:40 -0600

    Phase 6: Updated for new functionality

--- client/src/main/java/ServerFacade/WSFacade.java
+++ client/src/main/java/ServerFacade/WSFacade.java
@@ -17,28 +17,27 @@ public class WSFacade extends Endpoint {
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         ServerFacade serverFacade = new ServerFacade();
         this.session = container.connectToServer(this, uri);
+        onOpen(null, null);
 
-        this.session.addMessageHandler(new MessageHandler.Whole<String>() {
-
-            public void onMessage(String message) {
-                ServerMessage tempMessage = new Gson().fromJson(message, ServerMessage.class);
-                switch (tempMessage.getServerMessageType()) {
-                    case ERROR:
-                        System.out.println(new Gson().fromJson(message, Error.class).getMessage());
-                        break;
-                    case NOTIFICATION:
-                        System.out.println(new Gson().fromJson(message, Notification.class).getMessage());
-                        break;
-                    case LOAD_GAME:
-                        try {
-                            serverFacade.redrawBoard();
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                        break;
-                }
+        this.session.addMessageHandler((MessageHandler.Whole<String>) message -> {
+            ServerMessage tempMessage = new Gson().fromJson(message, ServerMessage.class);
+            switch (tempMessage.getServerMessageType()) {
+                case ERROR:
+                    System.out.println(new Gson().fromJson(message, Error.class).getMessage());
+                    break;
+                case NOTIFICATION:
+                    System.out.println(new Gson().fromJson(message, Notification.class).getMessage());
+                    break;
+                case LOAD_GAME:
+                    try {
+                        serverFacade.redrawBoard();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
             }
         });
+
     }
 
     public void send(String command) throws Exception {
@@ -47,7 +46,6 @@ public class WSFacade extends Endpoint {
     }
 
     public void onOpen(Session session, EndpointConfig endpointConfig) {
-        this.session = session;
     }
 
 }
